934eea52f0af3a14f4a48fd535067f82a2164c4a
Christopher-OOC <olojedechristopher24@gmail.com>
2024-12-18 21:43:38 +0100

    commit

---
+++ src/main/java/com/appdevelopersblog/ws/ProductsMicroservice/service/DispatchService.java
@@ -0,0 +1,14 @@
+package com.appdevelopersblog.ws.ProductsMicroservice.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DispatchService {
+
+    public void process(String patload) {
+
+    }
+
+}
